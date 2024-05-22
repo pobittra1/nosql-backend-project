c97acad169cf679a8b0e38975f7f7b151805cb03
pobi <pobittrachandraroy2001@gmail.com>
2024-05-23 04:48:20 +0600

    create order interface and model with schema

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+import { TOrder } from './order.interface';
+
+const orderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+    required: [true, 'you should provide email of order'],
+    trim: true,
+  },
+  productId: {
+    type: String,
+    required: [true, 'you should provide productId of order'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'you should provide price of order'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'you should provide price of order'],
+  },
+});
+
+//create model
+const Order = model('Product', orderSchema);
+
+export default Order;
