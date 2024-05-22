79c2e14c1c94b90980bb78abb67475b78cc6acb8
pobi <pobittrachandraroy2001@gmail.com>
2024-05-22 11:52:29 +0600

    install successfuly eslint after facing some errors

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+//destructuring from config file
+const { port, database_url } = config;
+
+async function main() {
+  await mongoose.connect(database_url as string);
+  try {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
